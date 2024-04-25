203a0790e4f34d35d2e1980c4d2da139209fd7b1
Surendravarmaa <jampana.varma@ad.infosys.com>
2024-04-25 15:50:37 +0530

    function

--- mysql.sh
+++ mysql.sh
@@ -45,7 +45,7 @@ then
     mysql_secure_installation --set-root-pass ${MYSQL_ROOT_PASSWORD} &>>$LOGFILE
     VALIDATE $? "MySQL Root password setup"
 else
-    echo "Already mysql root password is set...$Y SKIPPING $N"
+    echo -e "Already mysql root password is set...$Y SKIPPING $N"
 fi
 
 #mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
